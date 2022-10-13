24254c88596b564505f6072613e0a38cc50a7d21
jaysmith13 <janeasmith13@gmail.com>
2022-10-12 20:55:37 -0600

    general update
    update/ error fix/ ignore add

--- mad_libs_generator/main.py
+++ mad_libs_generator/main.py
@@ -1,16 +1,50 @@
-from importlib.resources import path
 import json
 import os
-from re import template
-from typing_extensions import Self
+
 
 class MadLibs:
     path = "./templates"
-    def_init_(self, word_descriptions, template);
+    def __init__(self, word_descriptions, template):
         self.template = template
-        self.word_description = word_descriptions
+        self.word_descriptions = word_descriptions
         self.user_input = []
         self.story = None
 
-@classmethod
-def from_json
+    @classmethod
+    def from_json(cls, name, path=None):
+        if not path:
+            path = cls.path
+        fpath = os.path.join(path, name)
+        with open(fpath, "r") as f:
+            data = json.load(f)
+        mad_lib = cls(**data)
+        return mad_lib
+
+    def get_words_from_user(self):
+        print("Please provide the following words: ")
+        for desc in self.word_descriptions:
+            ui = input(desc + " ")
+            self.user_input.append(ui)
+        return self.user_input
+
+    def build_story(self):
+        self.story = self.template.format(*self.user_input)
+        return self.story
+
+    def show_story(self):
+        print(story)
+
+
+def select_template():
+    print("Select a Mad Lib from the following list:")
+    templates = os.listdir(MadLibs.path)
+    template = input(str(templates) + " ")
+    return template
+
+
+temp_name = select_template()
+# temp_name = "day_at_the_zoo.json"
+mad_lib = MadLibs.from_json(temp_name)
+words = mad_lib.get_words_from_user()
+story = mad_lib.build_story()
+mad_lib.show_story()
